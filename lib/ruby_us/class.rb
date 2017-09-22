3a73f4c219741dc9df47429c41215402d2aad19c
Rafael Laurindo <rplaurindo>
2017-09-22 02:07:10 -0300

    fix Class and add "descendants method"

--- lib/ruby_us/class.rb
+++ lib/ruby_us/class.rb
@@ -1,5 +1,17 @@
 class Class
-  def extends? constant
-    ancestors.include?(constant) if constant != self
+
+  def extend? constant
+    (ancestors - [self]).include? constant
   end
+
+  def descendants
+    ObjectSpace.each_object(Class).select do |cls|
+      cls.extend? self
+    end
+  end
+
+  def extended_by? constant
+    descendants.include? constant
+  end
+
 end
